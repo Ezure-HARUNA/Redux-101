338e0486ba03d24bc3e91aa78f57b5328026c11f
ezure haruna <burimofumofu@gmail.com>
2020-09-29 16:17:41 +0900

    multiple-states

--- src/App.js
+++ src/App.js
@@ -1,61 +1,26 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
-  const increment = () => {
-    setCount(count + 1)
+  const initialStates = {
+    name: 'サンプル',
+    price: 1000
   }
 
-  const decrement = () => {
-    setCount(count - 1)
-  }
-
-  const increment2 = () => {
-    setCount(prexiousCount => prexiousCount + 1)
-  }
-
-  const decrement2 = () => {
-    setCount(prexiousCount => prexiousCount - 1)
-  }
-
-  const double = () => {
-    setCount(count * 2)
-  }
-
-  const ifTriple = () => {
-    if (count % 3 === 0) {
-      return setCount(count / 3)
-    } else {
-      return setCount(count)
-    }
-  }
-
+  const [name, setName] = useState(initialStates.name)
+  const [price, setPrice] = useState(initialStates.price)
+    
   const reset = () => {
-    setCount(0)
+    setPrice(initialStates.price)
+    setName(initialStates.name)
   }
+
   return (
     <React.Fragment>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={double}>×2</button>
-      </div>
-      <div>
-        <button onClick={ifTriple}>3の倍数の時だけ3で割る</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}></input>
     </React.Fragment>
   );
 }
