a6d7a9aab8b847f2ff7386603659de04cab38101
ezure haruna <burimofumofu@gmail.com>
2020-09-29 19:15:25 +0900

    implement event create

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -23,20 +23,21 @@
 //      }
 // ]
 
+
 const events = (state = [], action) => {
-    swith(action.type) {
-        case 'CREATE_EVENT':
-          const event = { title: action.title, body: action.body}
-          const length = state.length
-          let id = length === 0 ?  1 : id = state[length -1].id +1}
-          return ...state, { id: id, ...event}
-        case 'DELETE_EVENT':
-          return state
-        case 'DELETE_ALL_EVENTS':
-          return stete
-        default:
-            return state
-    }
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body }
+      const length = state.length
+      const id = length === 0 ? 1 : state[length - 1].id + 1
+      return [...state, { id, ...event }]
+    case 'DELETE_EVENT':
+      return state
+    case 'DELETE_ALL_EVENTS':
+      return []
+    default:
+      return state
+  }
 }
 
 export default events
